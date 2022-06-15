fba549d9c0443f6a21611e5be467b833644fe543
Gurkan <hasankaraman_tr@hotmail.com>
2022-06-15 20:31:41 +0300

    tcp

--- intelligence.py
+++ intelligence.py
@@ -2,60 +2,55 @@ from genetic import Individual, Genetic
 import core
 from core import Sequential
 from core import Conv2D, Dense, ReLu, Sigmoid, Softmax
-
+import tcpConnection
+from tcpConnection import User
+import controller
 from PIL import Image
-
 import numpy as np
-X = np.random.rand(64, 56)
 
-brain1 = Sequential()
-brain1.add(Conv2D(input_shape = X.shape))
-brain1.add(Sigmoid())
-brain1.add(Dense(32))
-brain1.add(Sigmoid())
-brain1.add(Dense(8))
-brain1.add(Sigmoid())
-brain1.add(Dense(4))
-brain1.add(Softmax())
+image_counter = 0
 
-brain1.initialize_weights()
+SHAPE = (32, 28)
+POP_SIZE = 10
 
-print("====== BIR =====")
-y = brain1.forward(X)
-for i in y:
-    for j in i:
-        print(j)
+intelligence_user = User("AI", 1235)
+intelligence_user.startListen()
+intelligence_user.clientConnect(tcpConnection._server, 1234)
 
-brain2 = Sequential()
-brain2.add(Conv2D(input_shape = X.shape))
-brain2.add(Sigmoid())
-brain2.add(Dense(32))
-brain2.add(Sigmoid())
-brain2.add(Dense(8))
-brain2.add(Sigmoid())
-brain2.add(Dense(4))
-brain2.add(Softmax())
+def getLastImage():
+    global image_counter
+    if image_counter > 50:
+        image_counter = 0
+    image_text = "0"*(3-len(str(image_counter)))+str(image_counter)
+    image = Image.open(r".\game\Screenshots\mario"+f"{image_text}.png").convert("L")
+    image = image.resize(SHAPE)
+    image_counter += 1
+    return image
 
-brain2.initialize_weights()
+def initialize_pop():
+    pop = []
+    for _ in range(POP_SIZE):
+        brain = Sequential()
+        brain.add(Conv2D(input_shape = SHAPE))
+        brain.add(Sigmoid())
+        brain.add(Dense(32))
+        brain.add(Sigmoid())
+        brain.add(Dense(8))
+        brain.add(Sigmoid())
+        brain.add(Dense(4))
+        brain.add(Softmax())
+        brain.initialize_weights()
 
-print("====== IKI =====")
-y = brain2.forward(X)
-for i in y:
-    for j in i:
-        print(j)
+        pop.append(Individual(brain))
+    return pop
 
-new_brain = Genetic().crossover(brain1, brain2)
+population = initialize_pop()
+next_gen = []
 
-print("====== NEW =====")
-y = new_brain.forward(X)
-for i in y:
-    for j in i:
-        print(j)
-
-img = Image.open(r"C:\Users\hasan\Desktop\genetic_mario\game\Screenshots\mario000.png").convert("L")
-img = img.resize((128, 112))
-
-out = Sequential().convolve(np.array(img), Conv2D((64, 56)).filters)
-
-for i in range(10):
-    Image.fromarray(out[:, :, i]).show()
+while True:
+    img = getLastImage()
+    brain = population.pop()
+    y = np.argmax(brain.forward(np.array(img)))
+    intelligence_user.sendMessage(y)
+    if(intelligence_user.messageBox.pop() == "siyah"):
+        break
