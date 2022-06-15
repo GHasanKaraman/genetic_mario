05830cbf25854772143e27aa6d839176d9eef822
Gurkan <hasankaraman_tr@hotmail.com>
2022-06-15 22:06:35 +0300

    tcp data fixed

--- tcpConnection.py
+++ tcpConnection.py
@@ -13,7 +13,7 @@ class User:
     def __init__(self, clientName, port):
         self.clientName = clientName
         self.port = port
-        self.messageBox = []
+        self.data = ""
 
     def startListen(self):
         try:
@@ -31,28 +31,26 @@ class User:
         while True:
             client, address = self.server.accept()
 
-            thread1 = threading.Thread(target=self.getMessage, args=(client,))
+            thread1 = threading.Thread(target=self.getData, args=(client,))
             thread1.start()
 
-    def getMessage(self, client):
+    def getData(self, client):
         while True:
             try:
-                msg = pickle.loads(client.recv(1024))
-                self.messageBox.append(msg)
-                if(len(self.messageBox) > 5):
-                    self.messageBox.pop(0)
+                data = pickle.loads(client.recv(1024))
+                self.data = data
             except:
                 client.close()
                 break
 
-    def sendMessage(self, msg):
+    def sendData(self, msg):
         self.client.send(pickle.dumps(msg))
         time.sleep(0.1)
 
     def clientConnect(self, server, port):
         try:
             self.client.connect((server, port))
-            thread1 = threading.Thread(target=self.getMessage, args=(self.client,))
+            thread1 = threading.Thread(target=self.getData, args=(self.client,))
             thread1.start()
             return True
         except:
