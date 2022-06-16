1281a0c2aa94a1c998038d847c9d8bae0cb07d58
Gurkan <hasankaraman_tr@hotmail.com>
2022-06-16 19:32:49 +0300

    controller deleted

--- intelligence.py
+++ intelligence.py
@@ -2,21 +2,50 @@ from genetic import Individual, Genetic
 import core
 from core import Sequential
 from core import Conv2D, Dense, ReLu, Sigmoid, Softmax
-from tcpConnection import User, _server
 import numpy as np
 import time
 
+from pywinauto import keyboard
+from pywinauto import findwindows
+import cv2
+import numpy as np
+import os
+import subprocess
+import cv2
+
 from bizhook import Memory
-combined_wram = Memory('Combined WRAM')
-newX = 0
-oldX = 0
 
-ai_user = User("ai_user", 1235)
-ai_user.startListen()
-while(ai_user.clientConnect(_server, 1234) == False):
-    print("There is no Controller") 
+# arrow keys, A=V, B=C, Y=X, X=D, START=SPACE, SELECT=ENTER, L=A, R=S, loadState = F1, screenShot = F12
+inputDict = {
+    0: "LEFT",
+    1: "RIGHT",
+    2: "DOWN",
+    3: "x",
+}
+path = "./game/SNES/Screenshots/"
+lastKey = ""
 
-image_counter = 0
+def controller(index):
+    global lastKey
+    if(lastKey != ""):
+        keyboard.send_keys("{"+lastKey+" up}")
+
+    lastKey = inputDict[index]
+    keyboard.send_keys("{"+lastKey+" down}")
+
+def getSS():
+    ssList = os.listdir(path)
+    if(len(ssList) > 0):
+        for i in ssList:
+            os.remove(path + i)
+
+    keyboard.send_keys("{F12}")
+    time.sleep(0.2)
+    last_image_array = cv2.imread(path + os.listdir(path)[0], 0)
+    if(np.sum(last_image_array) == 0):
+        keyboard.send_keys("{F1}")
+    else:
+        return last_image_array
 
 SHAPE = (256, 224)
 POP_SIZE = 10
@@ -41,16 +70,21 @@ def initialize_pop():
 population = initialize_pop()
 next_gen = []
 
+combined_wram = Memory('Combined WRAM')
+newX = 0
+oldX = 0
 while True:
-    if(ai_user.dataChanged):
+    try:
+        findwindows.find_window(
+                    title="Super Mario World (USA) [SNES] - BizHawk")
+        image = getSS()
         oldX = newX
+        print("ASD")
         newX = combined_wram.read_s16_le(0x94)
-        if newX == oldX:
-            print("DURMA KARDES!!!")
+        print(newX, oldX)
         time.sleep(0.1)
-        ai_user.dataChanged = False
-        image = ai_user.data
         brain = population[0].model
         y = brain.forward(np.array(image))
-        print(y)
-        ai_user.sendData(y)
+        controller(np.argmax(y))
+    except:
+        pass
