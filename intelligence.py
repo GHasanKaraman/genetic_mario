2b4c90644819cd53830e0bb84b86fee717e666f8
Gurkan <hasankaraman_tr@hotmail.com>
2022-06-16 18:57:33 +0300

    intelligence with bizhook

--- intelligence.py
+++ intelligence.py
@@ -4,6 +4,12 @@ from core import Sequential
 from core import Conv2D, Dense, ReLu, Sigmoid, Softmax
 from tcpConnection import User, _server
 import numpy as np
+import time
+
+from bizhook import Memory
+combined_wram = Memory('Combined WRAM')
+newX = 0
+oldX = 0
 
 ai_user = User("ai_user", 1235)
 ai_user.startListen()
@@ -37,6 +43,11 @@ next_gen = []
 
 while True:
     if(ai_user.dataChanged):
+        oldX = newX
+        newX = combined_wram.read_s16_le(0x94)
+        if newX == oldX:
+            print("DURMA KARDES!!!")
+        time.sleep(0.1)
         ai_user.dataChanged = False
         image = ai_user.data
         brain = population[0].model
